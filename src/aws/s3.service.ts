df7d02e75bb8cc7b1631687f48d49a875ea06c06
ohansck <ohansck@gmail.com>
2024-06-06 00:06:18 +0100

    add exec child process for s3

--- src/aws/s3.service.ts
+++ src/aws/s3.service.ts
@@ -1,5 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { S3Client, ListBucketsCommand, HeadBucketCommand, GetBucketLocationCommand } from '@aws-sdk/client-s3';
+import { execSync } from 'child_process';
+
+function exec(cmd: string): string {
+    return execSync(cmd).toString().trim();
+}
 
 @Injectable()
 export class S3Service {
@@ -11,34 +16,34 @@ export class S3Service {
 
     async listBuckets(): Promise<string[]> {
         try {
-            const data = await this.s3Client.send(new ListBucketsCommand({}));
-            return data.Buckets.map(bucket => bucket.Name);
+            const res = JSON.parse(exec('aws s3api list-buckets'));
+            //const data = await this.s3Client.send(new ListBucketsCommand({}));
+            return res.Buckets.map(bucket => bucket.Name);
         } catch (error) {
             this.logger.error('Error listing buckets:', error);
             throw error;
         }
     }
 
-    async bucketExists(bucketName: string): Promise<boolean> {
+    async bucketExists(bucketName: string): Promise<{ message: string }> {
         try {
-            await this.s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
-            return true;
+            //await this.s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
+            exec(`aws s3api head-bucket --bucket ${bucketName}`);
+            return { message: `Bucket ${bucketName} exists` };
         } catch (error) {
-            if (error.name === 'NotFound') {
-                return false;
-            }
             this.logger.error('Error checking if bucket exists:', error);
-            throw error;
+            return { message: `Bucket ${bucketName} does not exist` };
         }
     }
 
     async getBucketDetails(bucketName: string): Promise<any> {
         try {
-            const headData = await this.s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
+            //const headData = await this.s3Client.send(new HeadBucketCommand({ Bucket: bucketName }));
             // Add more details if necessary
+            const res = JSON.parse(exec(`aws s3api get-bucket-ownership-controls --bucket ${bucketName}`));
             return {
-                BucketName: bucketName,
-                BucketData: headData,
+                Type: 'Bucket Ownership Control',
+                Data: res,
             };
         } catch (error) {
             this.logger.error('Error getting bucket details:', error);
